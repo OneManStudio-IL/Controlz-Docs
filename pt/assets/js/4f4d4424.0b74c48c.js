c9b07932e8718ee61708c25f750bcf4b6de9fd23
OneManStudio-IL <onemanstudio.co.il@gmail.com>
2025-06-04 21:41:13 +0000

    Deploying to gh-pages from @ OneManStudio-IL/Controlz-Docs@c5d7b3300bef3a8cdfca7cfb631587fb1d23aa07 🚀

---
+++ pt/assets/js/4f4d4424.0b74c48c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcontrolz_docs=self.webpackChunkcontrolz_docs||[]).push([[995],{2753:e=>{e.exports=JSON.parse('{"tags":[{"label":"Dev","permalink":"/Controlz-Docs/pt/blog/tags/dev","description":"Development news","count":5},{"label":"Test","permalink":"/Controlz-Docs/pt/blog/tags/test","description":"Testing news","count":5}]}')}}]);
